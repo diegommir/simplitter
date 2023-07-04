bde3de0f1ce3b9abca62c72810a0f5ea636cb352
¨Diego <diegommir@gmail.com>
2023-07-04 15:19:37 +1000

    Person update

--- server/core/views.py
+++ server/core/views.py
@@ -1,11 +1,11 @@
 """This module contains all the API endpoints used in this app."""
 
-from django.http import JsonResponse
+from django.db.models import Q
 from rest_framework import views, status
-from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from .serializers import PersonSerializer
 from .models import Person
+from rest_framework.viewsets import ModelViewSet
 
 class PersonAPI(views.APIView):
     """
@@ -50,19 +50,33 @@ class PersonIdAPI(views.APIView):
     def get(self, request, id):
         """Return one Person from database based on the given id."""
         try:
-            person = Person.objects.get(id=id)
+            person = Person.objects.get(Q(id=id) & Q(status=Person.Status.ACTIVE))
 
             serializer = PersonSerializer(person)
             return Response(serializer.data)
         except Person.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
     
-    def delete(self, request, id):
-        """Delete one Person from the database based on the given id."""
+    def patch(self, request, id):
+        """Update a Person given the id."""
         try:
-            person = Person.objects.get(id=id)
-            person.delete()
+            person = Person.objects.get(Q(id=id) & Q(status=Person.Status.ACTIVE))
 
-            return Response({"message":f"Person {person.username} deleted successfully."})
+            serializer = PersonSerializer(instance=person, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Person.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    
+    def delete(self, request, id):
+        """Change status of a Person to DELETED based on the given id."""
+        try:
+            person = Person.objects.get(Q(id=id) & Q(status=Person.Status.ACTIVE))
+
+            return Response({"message":f"Person {person.username} updated successfully."})
         except Person.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
